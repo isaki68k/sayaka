bd1ad39df022dc60aaf95063c51d81011a401d5a
Tetsuya Isaki <isaki@pastel-flower.jp>
2014-10-01 17:23:40 +0900

    img2sixel がパス上になければ自動的に無視する。

--- sayaka.php
+++ sayaka.php
@@ -139,17 +139,19 @@ function init_stream()
 
 	// img2sixel
 	// XXX sixelなしモードを引数で指定したい
-	$img2sixel = "img2sixel";
-	if ($color_mode <= 16) {
-		if ($color_mode == 2) {
-			$img2sixel .= " -e --quality=low";
-		} else {
-			$file = "colormap{$color_mode}.png";
-			if (file_exists($file)) {
-				$img2sixel .= " -m {$file}";
+	$img2sixel = rtrim(`which img2sixel`);
+	if ($img2sixel != "") {
+		if ($color_mode <= 16) {
+			if ($color_mode == 2) {
+				$img2sixel .= " -e --quality=low";
 			} else {
-				print "No colormap file: {$file}\n";
-				exit(1);
+				$file = "colormap{$color_mode}.png";
+				if (file_exists($file)) {
+					$img2sixel .= " -m {$file}";
+				} else {
+					print "No colormap file: {$file}\n";
+					exit(1);
+				}
 			}
 		}
 	}
