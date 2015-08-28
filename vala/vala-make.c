a9f69e090c5cd9d93a3bf5194e943aec2f2c4901
Tetsuya Isaki <isaki@pastel-flower.jp>
2015-08-28 14:59:41 +0900

    .vala/.c/.o の依存関係が Make だけでは解消できないので、 vala-make で全部やってみた。

--- vala/vala-make.c
+++ vala/vala-make.c
@@ -26,30 +26,43 @@
 void usage(void) __attribute__((__noreturn__));
 
 int debug;
+int dry_run;
+int echocmd;
 
 int
 main(int ac, char *av[])
 {
-	struct stat stv;
-	struct stat stc;
+	char cmd[1024];	// 適当
 	const char *vala_cmd;
+	const char *cc_cmd;
+	const char *ld_cmd;
+	const char *libs;
+	const char *exefile;
+	char **cfiles;
+	char **ofiles;
 	time_t target_mtime;
 	int updated;
+	int *updates;
 	int c;
 	int i;
 	int r;
-	int dry_run;
-	int echocmd;
 
-	vala_cmd = "valac";
+	vala_cmd = "valac -C";
+	cc_cmd = "cc";
+	ld_cmd = "cc";
+	libs = "";
+	exefile = "a.out";
 	updated = 0;
 	dry_run = 0;
 	echocmd = 0;
 
-	while ((c = getopt(ac, av, "c:den")) != -1) {
+	while ((c = getopt(ac, av, "a:c:del:L:no:")) != -1) {
 		switch (c) {
-		 case 'c':
+		 case 'a':
 			vala_cmd = optarg;
+			break;
+		 case 'c':
+			cc_cmd = optarg;
 			break;
 		 case 'd':
 			debug = 1;
@@ -57,8 +70,17 @@ main(int ac, char *av[])
 		 case 'e':
 			echocmd = 1;
 			break;
+		 case 'l':
+			ld_cmd = optarg;
+			break;
+		 case 'L':
+			libs = optarg;
+			break;
 		 case 'n':
 			dry_run = 1;
+			break;
+		 case 'o':
+			exefile = optarg;
 			break;
 		 default:
 			usage();
@@ -72,70 +94,176 @@ main(int ac, char *av[])
 		usage();
 	}
 
-	// srcs... についてタイムスタンプを比較
+	cfiles = malloc(ac * sizeof(char*));
+	if (cfiles == NULL) {
+		err(1, "malloc");
+	}
+	ofiles = malloc(ac * sizeof(char*));
+	if (ofiles == NULL) {
+		err(1, "malloc");
+	}
+	updates = calloc(ac, sizeof(int));
+	if (updates == NULL) {
+		err(1, "malloc");
+	}
+
+	// .vala と同じだけの .c と .o ファイル名を作成
 	for (i = 0; i < ac; i++) {
 		const char *valafile = av[i];
-		char cfile[PATH_MAX];
+		char filename[PATH_MAX];
 
 		// foo.vala -> foo.c
-		strlcpy(cfile, valafile, sizeof(cfile));
-		char *p = strstr(cfile, ".vala");
+		strlcpy(filename, valafile, sizeof(filename));
+		char *p = strstr(filename, ".vala");
 		if (p == NULL) {
 			errx(1, "<srcs> should have .vala extension");
 		}
+
 		strcpy(p, ".c");
-		if (debug) {
-			printf("checking %s and %s .. ", valafile, cfile);
-			fflush(stdout);
-		}
+		cfiles[i] = strdup(filename);
 
-		r = stat(valafile, &stv);
-		if (r == -1) {
-			err(1, "stat: %s", valafile);
+		strcpy(p, ".o");
+		ofiles[i] = strdup(filename);
+	}
+
+	//
+	// .vala -> .c
+	//
+
+	// .vala vs .c についてタイムスタンプをすべて比較
+	// どの C ソースが更新されたか個別に把握するので、早期終了しない。
+	updated = 0;
+	for (i = 0; i < ac; i++) {
+		const char *valafile = av[i];
+		const char *cfile = cfiles[i];
+
+		if (need_update(valafile, cfile) == 1) {
+			updates[i] = 1;
+			updated++;
 		}
-		r = stat(cfile, &stc);
-		if (r == -1) {
-			if (errno != ENOENT) {
-				err(1, "stat: %s", cfile);
+	}
+
+	// 必要なら valac 実行
+	if (updated) {
+		strcpy(cmd, vala_cmd);
+		for (i = 0; i < ac; i++) {
+			strlcat(cmd, " ", sizeof(cmd));
+			strlcat(cmd, av[i], sizeof(cmd));
+		}
+		run_cmd(cmd);
+
+		// .c のタイムスタンプを更新
+		for (i = 0; i < ac; i++) {
+			if (updates[i]) {
+				if (debug) {
+					printf("touching %s\n", cfiles[i]);
+				}
+				if (utimes(cfiles[i], NULL) == -1) {
+					err(1, "utimes: %s", cfiles[i]);
+				}
 			}
 		}
+	}
 
-		if (stv.st_mtime > stc.st_mtime) {
-			updated = 1;
-			if (debug) {
-				printf("need-to-update\n");
-			}
+	//
+	// .c -> .o
+	//
+
+	// タイムスタンプを比較して、それぞれコンパイル
+	updated = 0;
+	for (i = 0; i < ac; i++) {
+		updates[i] = 0;
+	}
+	for (i = 0; i < ac; i++) {
+		const char *cfile = cfiles[i];
+		const char *ofile = ofiles[i];
+
+		if (need_update(cfile, ofile) == 1) {
+			snprintf(cmd, sizeof(cmd), "%s -c %s -o %s", cc_cmd, cfile, ofile);
+			run_cmd(cmd);
+		}
+	}
+
+	//
+	// .o -> target_exe
+	//
+
+	// .o と exe を比較
+	updated = 0;
+	for (i = 0; i < ac; i++) {
+		const char *ofile = ofiles[i];
+		if (need_update(ofile, exefile) == 1) {
+			updated++;
 			break;
 		}
-		if (debug) {
+	}
+	if (updated) {
+		sprintf(cmd, "%s -o %s", ld_cmd, exefile);
+		for (i = 0; i < ac; i++) {
+			strlcat(cmd, " ", sizeof(cmd));
+			strlcat(cmd, ofiles[i], sizeof(cmd));
+		}
+		strlcat(cmd, " ", sizeof(cmd));
+		strlcat(cmd, libs, sizeof(cmd));
+
+		run_cmd(cmd);
+	}
+
+	return 0;
+}
+
+// srcfile が dstfile より新しければ 1 を返す。
+int
+need_update(const char *srcfile, const char *dstfile)
+{
+	struct stat st1;
+	struct stat st2;
+	int r;
+
+	if (debug) {
+		printf("checking %s and %s .. ", srcfile, dstfile);
+		fflush(stdout);
+	}
+
+	r = stat(srcfile, &st1);
+	if (r == -1) {
+		err(1, "stat: %s", srcfile);
+	}
+
+	memset(&st2, 0, sizeof(st2));
+	r = stat(dstfile, &st2);
+	if (r == -1) {
+		if (errno != ENOENT) {
+			err(1, "stat: %s", dstfile);
+		}
+	}
+
+	if (st1.st_mtime > st2.st_mtime) {
+		r = 1;
+	}
+	if (debug) {
+		if (r) {
+			printf("need-to-update\n");
+		} else {
 			printf("no-update\n");
 		}
 	}
+	return r;
+}
 
-	// 更新なければここで終わり
-	if (updated == 0) {
-		return 0;
-	}
-
-	char cmd[1024];	// 適当
-	strcpy(cmd, vala_cmd);
-	for (i = 0; i < ac; i++) {
-		strlcat(cmd, " ", sizeof(cmd));
-		strlcat(cmd, av[i], sizeof(cmd));
-	}
-
-	if (debug) {
-		printf("cmd=|%s|\n", cmd);
-	}
+void
+run_cmd(const char *cmd)
+{
 	if (dry_run) {
 		printf("%s\n", cmd);
 	} else {
 		if (echocmd) {
 			printf("%s\n", cmd);
 		}
-		system(cmd);
+		if (system(cmd) != 0) {
+			exit(1);
+		}
 	}
-	return 0;
 }
 
 void __attribute__((__noreturn__))
