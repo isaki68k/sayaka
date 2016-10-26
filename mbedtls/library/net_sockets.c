0d912b2594dbf5a7fa1eba5e4865f22fd666491f
Tetsuya Isaki <isaki@pastel-flower.jp>
2016-10-26 19:51:35 +0900

    NetBSD では socklen_t が定義されている。これでワーニングが消える。

--- mbedtls/library/net_sockets.c
+++ mbedtls/library/net_sockets.c
@@ -302,6 +302,7 @@ int mbedtls_net_accept( mbedtls_net_context *bind_ctx,
     struct sockaddr_storage client_addr;
 
 #if defined(__socklen_t_defined) || defined(_SOCKLEN_T) ||  \
+	defined(socklen_t) || \
     defined(_SOCKLEN_T_DECLARED) || defined(__DEFINED_socklen_t)
     socklen_t n = (socklen_t) sizeof( client_addr );
     socklen_t type_len = (socklen_t) sizeof( type );
