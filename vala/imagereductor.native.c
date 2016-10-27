593ea411980833f37b106bd2abb52dc08cb5501b
moveccr <moveccr@>
2016-10-27 15:19:24 +0900

    fast 変換ルーチンのループから除算を排除した。

--- vala/imagereductor.native.c
+++ vala/imagereductor.native.c
@@ -24,24 +24,30 @@ StepRationalCreate(int i, int n, int d)
 {
 	StepRational rv;
 	rv.I = i;
-	rv.N = n;
+	if (n < d) {
+		rv.N = n;
+	} else {
+		rv.I += n / d;
+		rv.N = n % d;
+	}
 	rv.D = d;
 	return rv;
 }
 
 static void
-StepRationalAddStep(StepRational* sr, int nStep)
+StepRationalAdd(StepRational* sr, StepRational* x)
 {
-	sr->N += nStep;
-	while (sr->N < 0) {
+	sr->I += x->I;
+	sr->N += x->N;
+	if (sr->N < 0) {
 		sr->I--;
 		sr->N += sr->D;
-	}
-	while (sr->N >= sr->D) {
+	} else if (sr->N >= sr->D) {
 		sr->I++;
 		sr->N -= sr->D;
 	}
 }
+
 
 static int
 imagereductor_findfixed8(uint8_t r, uint8_t g, uint8_t b)
@@ -60,6 +66,18 @@ saturate_byte(int x)
 	return (uint8_t)x;
 }
 
+static int
+RoundDownPow2(int x)
+{
+	x |= x >> 1;
+	x |= x >> 2;
+	x |= x >> 4;
+	x |= x >> 8;
+	x |= x >> 16;
+	x += 1;
+	return x >> 1;
+}
+
 int
 imagereductor_resize_reduce_fast_fixed8(
 	uint8_t *dst, int dstLen,
@@ -68,7 +86,7 @@ imagereductor_resize_reduce_fast_fixed8(
 	int srcWidth, int srcHeight,
 	int srcNch, int srcStride)
 {
-// 水平方向は平均
+// 水平方向は Pow2 になるピクセルをサンプリングして平均
 // 垂直方向はスキップサンプリング
 
 //fprintf(stderr, "dst=(%d,%d) src=(%d,%d)\n", dstWidth, dstHeight, srcWidth, srcHeight);
@@ -76,39 +94,37 @@ imagereductor_resize_reduce_fast_fixed8(
 	colorRGBint col;
 	col.r = col.g = col.b = 0;
 	StepRational sr_y = StepRationalCreate(0, 0, dstHeight);
+	StepRational sr_ystep = StepRationalCreate(0, srcHeight, dstHeight);
+
+	StepRational sr_x = StepRationalCreate(0, 0, dstWidth);
+	StepRational sr_xstep = StepRationalCreate(0, srcWidth, dstWidth);
+
+	int sw = RoundDownPow2(sr_xstep.I);
+	if (sw == 0) sw = 1;
+	int meanShift = 31 - __builtin_clz(sw);
 
 	for (int y = 0; y < dstHeight; y++) {
 		uint8_t *srcRaster = &src[sr_y.I * srcStride];
-//fprintf(stderr, "%d ", sr_y.I);
-		StepRationalAddStep(&sr_y, srcHeight);
-		StepRational sr_x = StepRationalCreate(0, 0, dstWidth);
+		StepRationalAdd(&sr_y, &sr_ystep);
+
+		sr_x.I = sr_x.N = 0;
+
 		for (int x = 0; x < dstWidth; x++) {
 
 			int sx0 = sr_x.I;
-			StepRationalAddStep(&sr_x, srcWidth);
-			int sx1 = sr_x.I;
-			if (sx0 == sx1) sx1 = sx0 + 1;
+			StepRationalAdd(&sr_x, &sr_xstep);
 
 			uint8_t *srcPix = &srcRaster[sx0 * srcNch];
-			for (int sx = sx0; sx < sx1; sx++) {
+			for (int sx = 0; sx < sw; sx++) {
 				col.r += srcPix[0];
 				col.g += srcPix[1];
 				col.b += srcPix[2];
 				srcPix += srcNch;	
 			}
 
-			int sumCount = sx1 - sx0;
-			if (sumCount <= 1) {
-				// nop
-			} else if (sumCount == 2) {
-				col.r /= 2;
-				col.g /= 2;
-				col.b /= 2;
-			} else {
-				col.r /= sumCount;
-				col.g /= sumCount;
-				col.b /= sumCount;
-			}
+			col.r >>= meanShift;
+			col.g >>= meanShift;
+			col.b >>= meanShift;
 
 			uint8_t f8 = imagereductor_findfixed8(
 				saturate_byte(col.r),
