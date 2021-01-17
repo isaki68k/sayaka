8c2741aa61675c694559b6b10f2a21c9315eea5a
isaki68k <isaki@pastel-flower.jp>
2021-01-17 13:11:48 +0900

    StreamBase.cpp 内で書きかけだった Fd{Input,Output}Stream を整備。 FdStream.cpp に分離して、とりあえず FdInputStream のみ。

---
+++ src/FdStream.cpp
@@ -0,0 +1,77 @@
+#include "FdStream.h"
+#include <unistd.h>
+
+//
+// ファイルディスクリプタ入力ストリーム
+//
+
+// コンストラクタ
+FdInputStream::FdInputStream(int fd_, bool own_)
+{
+	fd = fd_;
+	own = own_;
+}
+
+// デストラクタ
+FdInputStream::~FdInputStream()
+{
+	Close();
+}
+
+// 読み出し
+ssize_t
+FdInputStream::Read(char *buf, size_t buflen)
+{
+	return read(fd, buf, buflen);
+}
+
+// クローズ
+void
+FdInputStream::Close()
+{
+	if (own) {
+		if (fd >= 0) {
+			close(fd);
+		}
+		fd = -1;
+	}
+}
+
+#if 0 // notused
+//
+// ファイルディスクリプタ出力ストリーム
+//
+
+// コンストラクタ
+FdOutputStream::FdOutputStream(int fd_, bool own_)
+{
+	fd = fd_;
+	own = own_;
+}
+
+// デストラクタ
+FdOutputStream::~FdOutputStream()
+{
+	Close();
+}
+
+// 書き込み
+ssize_t
+FdOutputStream::Write(char *buf, size_t buflen)
+{
+	return write(fd, buf, buflen);
+}
+
+// クローズ
+void
+FdOutputStream::Close()
+{
+	if (own) {
+		if (fd >= 0) {
+			close(fd);
+		}
+		fd = -1;
+	}
+}
+
+#endif // notused
---
+++ src/FdStream.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "StreamBase.h"
+
+// ファイルディスクリプタ入力ストリーム
+class FdInputStream : public InputStream
+{
+	using inherited = InputStream;
+ public:
+	FdInputStream(int fd, bool own);
+	virtual ~FdInputStream() override;
+
+	ssize_t Read(char *buf, size_t buflen) override;
+	void Close() override;
+
+ private:
+	int fd {};
+	bool own {};
+};
+
+#if 0 // notused
+// ファイルディスクリプタ出力ストリーム
+class FdOutputStream : public OutputStream
+{
+	using inherited = OutputStream;
+ public:
+	FdOutputStream(int fd, const Diag& diag);
+	virtual ~FdOutputStream();
+
+	ssize_t Write(char *buf, size_t buflen) override;
+	void Close() override;
+
+ private:
+	int fd {};
+};
+#endif
