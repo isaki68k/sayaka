c81a8cce08b11676c2985e7cc619fc89ee9a4e81
Tetsuya Isaki <isaki@pastel-flower.jp>
2021-02-25 16:19:12 +0900

    StringUtil: stox32() を修正。

--- src/StringUtil.cpp
+++ src/StringUtil.cpp
@@ -354,9 +354,9 @@ stouT(const char *s, char **endp)
 			if ('0' <= c && c <= '9') {
 				c -= '0';
 			} else if ('A' <= c && c <= 'F') {
-				c -= 'A' + 10;
+				c = c - 'A' + 10;
 			} else if ('a' <= c && c <= 'f') {
-				c -= 'a' + 10;
+				c = c - 'a' + 10;
 			} else {
 				break;
 			}
