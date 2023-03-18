7e6473a3e8596c203ae611fbaf9ce0f63c00a597
isaki68k <isaki@pastel-flower.jp>
2023-03-18 14:20:22 +0900

    Update copyright year.

--- src/NGWord.cpp
+++ src/NGWord.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014-2021 Tetsuya Isaki
+ * Copyright (C) 2014-2022 Tetsuya Isaki
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
--- src/sayaka.cpp
+++ src/sayaka.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014-2021 Tetsuya Isaki
+ * Copyright (C) 2014-2023 Tetsuya Isaki
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
