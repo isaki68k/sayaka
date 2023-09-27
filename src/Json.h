882c9517bc6bbd3b404b52f0e84527bcc77c8dec
isaki68k <isaki@pastel-flower.jp>
2023-09-27 11:48:08 +0900

    Json.h: JsonAsString() マクロを用意してみる。 nlohmann-json だけでは確実に例外を出さずにオブジェクトから文字列を 取り出すのが面倒くさい。

--- src/Json.h
+++ src/Json.h
@@ -4,3 +4,19 @@
 #include "nlohmann/json.hpp"
 
 using Json = nlohmann::json;
+
+// JsonAsString(j) .. 文字列を取り出しやすくするマクロ。
+// j が string なら std::string を返す。
+// j がそれ以外なら empty を返す。
+//
+// json["key"] (Json の [] 演算子) は "key" が存在しない時は null 型
+// (Json(nullptr)) を返すので、合わせ技で
+// json = { "key":"value", "nullkey":null }
+// の場合に
+// JsonAsString(json["key"]) => "value"  // (1)
+// JsonAsString(json["nullkey"]) => ""   // (2)
+// JsonAsString(json["notexist"]) => ""  // (3)
+// となる。
+// Json::value(name, defval) は (1), (3) は動作するが (2) で例外を出すので
+// 使えない。
+#define JsonAsString(j) ((j).is_string() ? (j).get<std::string>() : "")
