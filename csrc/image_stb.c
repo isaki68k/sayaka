6dea03ab4fbc1938353f8eee1a5389b454273e2f
isaki68k <isaki@pastel-flower.jp>
2024-09-03 22:48:47 +0900

    csrc/image_stb: STBI_FAILURE_USERMSG は現状使ってないのでオフにする。

--- csrc/image_stb.c
+++ csrc/image_stb.c
@@ -54,7 +54,6 @@ _Pragma("GCC diagnostic ignored \"-Wunused-but-set-variable\"")
 #endif
 
 #define STB_IMAGE_IMPLEMENTATION
-#define STBI_FAILURE_USERMSG
 #include "stb/stb_image.h"
 
 #if defined(__clang__)
