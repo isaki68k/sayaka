e0784dddcfad13cafdc7d6974f5d55cd5aa36e26
isaki68k <isaki@pastel-flower.jp>
2024-09-05 15:17:12 +0900

    src/sayaka: --local が required_argument のままだったのを修正。 Fix issue#41.

--- src/sayaka.c
+++ src/sayaka.c
@@ -148,7 +148,7 @@ static const struct option longopts[] = {
 	{ "ipv6",			no_argument,		NULL,	OPT_ipv6 },
 	{ "jis",			no_argument,		NULL,	OPT_jis },
 	{ "light",			no_argument,		NULL,	OPT_light },
-	{ "local",			required_argument,	NULL,	'l' },
+	{ "local",			no_argument,		NULL,	'l' },
 	{ "mathalpha",		no_argument,		NULL,	OPT_mathalpha },
 	{ "max-image-cols",	required_argument,	NULL,	OPT_max_image_cols },
 	{ "misskey",		no_argument,		NULL,	OPT_misskey },
